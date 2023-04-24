dfe75c6449a4aa85c36b171cace870d076e9a9af
Yingyi <yingyihuang2000@outlook.com>
2023-04-24 21:52:07 +0800

    update trigger for insertion

--- trigger.sql
+++ trigger.sql
@@ -41,33 +41,40 @@ DELIMITER ;
 -- update player_season after insert a player_game record
 -- if exist: update
 -- otherwise: insert
--- How could we update the metrics in player_season_info after insert on game_player_info?
--- if 1) Could not solve --> delete it
 DELIMITER $$
 CREATE TRIGGER update_player_season_info AFTER INSERT ON game_player_info
 FOR EACH ROW
 BEGIN
-    DECLARE player_season_id INT;
     DECLARE player_season_count INT;
-    DECLARE team_id DATE;
     DECLARE season YEAR;
 
     SET @season := (SELECT season from game where game.GAME_ID=NEW.GAME_ID);
-    SET @player_season_count := SELECT COUNT(*) FROM player_season_info WHERE player_id = NEW.player_id;
+    SET @player_season_count := (SELECT COUNT(*) FROM player_season_info WHERE SEASON = @season AND PLAYER_ID=NEW.PLAYER_ID AND TEAM_ID=NEW.TEAM_ID);
 
-    IF player_season_count > 0 THEN
-            UPDATE player_season_info SET
-            ....?????
-        WHERE PLAYER_ID = NEW.PLAYER_ID AND TEAM_ID = NEW.TEAM_ID AND season = @season;
-    ELSE
-        INSERT INTO player_season_info (player_id, team_id, ......)
-        VALUES(
-            NEW.player_id,
-            NEW.team_id,
-            @season,
-            ...........???
-        )
+    IF player_season_count = 0 THEN
+        INSERT INTO player_season_info(PLAYER_ID, SEASON, TEAM_ID) VALUES(NEW.PLAYER_ID, @season, NEW.TEAM_ID);
+
     END IF;
+END; $$
+DELIMITER ;
+
+-- update player after insert a player_game record
+-- update attributes: there must be a corresponding player in player table
+DELIMITER $$
+CREATE TRIGGER update_player_info AFTER INSERT ON game_player_info
+FOR EACH ROW
+BEGIN
+    DECLARE GP INT;
+    DECLARE PTS FLOAT;
+    DECLARE REB FLOAT;
+    DECLARE AST FLOAT;
+
+    SET @GP = (SELECT GP from player where player.PLAYER_ID=NEW.PLAYER_ID);
+    SET @PTS = (SELECT PTS from player where player.PLAYER_ID=NEW.PLAYER_ID);
+    SET @AST = (SELECT AST from player where player.PLAYER_ID=NEW.PLAYER_ID);
+    SET @REB = (SELECT REB from player where player.PLAYER_ID=NEW.PLAYER_ID);
+
+    UPDATE player SET GP=@GP+1, PTS=(@PTS*@GP+NEW.PTS)/(@GP+1), REB=(@REB*@GP+NEW.REB)/(@GP+1), AST=(@AST*@GP+NEW.AST)/(@GP+1) WHERE PLAYER_ID=NEW.PLAYER_ID;
 END; $$
 DELIMITER ;
 
@@ -77,13 +84,54 @@ DELIMITER ;
 -- How could we update the metrics in player_season_info after insert on game_player_info?
 -- if 1) Could not solve --> delete it
 DELIMITER $$
-CREATE TRIGGER update_team_season_info AFTER INSERT ON game_player_info
+CREATE TRIGGER update_team_season_info AFTER INSERT ON game
 FOR EACH ROW
 BEGIN
-    DECLARE season_id INT;
-    DECLARE team_id INT;
-    
-    ????
+    DECLARE team_season_count INT;
+    DECLARE host_team_season_count INT;
+    DECLARE visitor_team_season_count INT;
+    DECLARE GP INT;
+    DECLARE W INT;
+    DECLARE L INT;
+
+    SET @team_season_count := (SELECT COUNT(*) FROM team_season_info WHERE SEASON = NEW.season AND (TEAM_ID=NEW.HOST_TEAM_ID OR TEAM_ID=NEW.VISITOR_TEAM_ID));
+    SET @host_team_season_count := (SELECT COUNT(*) FROM team_season_info WHERE SEASON = NEW.season AND TEAM_ID=NEW.HOST_TEAM_ID);
+    SET @visitor_team_season_count := (SELECT COUNT(*) FROM team_season_info WHERE SEASON = NEW.season AND TEAM_ID=NEW.VISITOR_TEAM_ID);
+
+    IF host_team_season_count = 0 THEN
+        IF NEW.HOME_TEAM_WIN = 1 THEN
+            INSERT INTO team_season_info(TEAM_ID, SEASON, GP, W, L) VALUES(NEW.HOST_TEAM_ID, NEW.SEASON, 1, 1, 0);
+        ELSE
+            INSERT INTO team_season_info(TEAM_ID, SEASON, GP, W, L) VALUES(NEW.HOST_TEAM_ID, NEW.SEASON, 1, 0, 1);
+        END IF;
+    ELSE 
+        SET @GP := (SELECT GP FROM team_season_info WHERE TEAM_ID=NEW.HOST_TEAM_ID AND SEASON=NEW.SEASON);
+        SET @W := (SELECT W FROM team_season_info WHERE TEAM_ID=NEW.HOST_TEAM_ID AND SEASON=NEW.SEASON);
+        SET @L := (SELECT L FROM team_season_info WHERE TEAM_ID=NEW.HOST_TEAM_ID AND SEASON=NEW.SEASON);
+        IF NEW.HOME_TEAM_WIN = 1 THEN
+            UPDATE team_season_info SET GP=@GP+1, W=@W+1 WHERE TEAM_ID=NEW.HOST_TEAM_ID AND SEASON=NEW.SEASON;
+        ELSE
+            UPDATE team_season_info SET GP=@GP+1, L=@L+1 WHERE TEAM_ID=NEW.HOST_TEAM_ID AND SEASON=NEW.SEASON;
+        END IF;
+    END IF;
+
+    IF visitor_team_season_count = 0 THEN
+        IF NEW.HOME_TEAM_WIN = 1 THEN
+            INSERT INTO team_season_info(TEAM_ID, SEASON, GP, W, L) VALUES(NEW.VISITOR_TEAM_ID, NEW.SEASON, 1, 0, 1);
+        ELSE
+            INSERT INTO team_season_info(TEAM_ID, SEASON, GP, W, L) VALUES(NEW.VISITOR_TEAM_ID, NEW.SEASON, 1, 0, 1);
+        END IF;
+    ELSE 
+        SET @GP := (SELECT GP FROM team_season_info WHERE TEAM_ID=NEW.VISITOR_TEAM_ID AND SEASON=NEW.SEASON);
+        SET @W := (SELECT W FROM team_season_info WHERE TEAM_ID=NEW.VISITOR_TEAM_ID AND SEASON=NEW.SEASON);
+        SET @L := (SELECT L FROM team_season_info WHERE TEAM_ID=NEW.VISITOR_TEAM_ID AND SEASON=NEW.SEASON);
+        IF NEW.HOME_TEAM_WIN = 1 THEN
+            UPDATE team_season_info SET GP=@GP+1, L=@L+1 WHERE TEAM_ID=NEW.VISITOR_TEAM_ID AND SEASON=NEW.SEASON;
+        ELSE
+            UPDATE team_season_info SET GP=@GP+1, W=@W+1 WHERE TEAM_ID=NEW.VISITOR_TEAM_ID AND SEASON=NEW.SEASON;
+        END IF;
+    END IF;
+
 END; $$
 DELIMITER ;
 
